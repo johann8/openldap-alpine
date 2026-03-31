4204a0b9a70aced550300b335566ae663cf59747
johann8 <jo.hahn@gmx.de>
2026-03-31 23:10:01 +0200

    small changes

--- build_image.sh
+++ build_image.sh
@@ -1,47 +1,44 @@
 #!/bin/bash
 
 # set variables
-_VERSION=0.1.3
-_TAG=alpine-openldap
+CUSTOM_VERSION=0.2.0
+CUSTOM_TAG=alpine-openldap
 
 # build image openldap
-DOCKER_BUILDKIT=0; docker build -t johann8/${_TAG}:${_VERSION} . 2>&1 | tee ./build.log
+DOCKER_BUILDKIT=0; 
+
+docker build \
+  --build-arg=OPENLDAP_VERSION=2.6.10-r0 \
+  --tag=johann8/${CUSTOM_TAG}:${CUSTOM_VERSION} \
+  --file=Dockerfile . 2>&1 | tee ./build.log
+
+# Result of build
 _BUILD=$?
+
+# If Build oK then tag docker image to latest
 if ! [ ${_BUILD} = 0 ]; then
    echo "ERROR: Docker Image build was not successful"
    exit 1
 else
    echo "Docker Image build successful"
    docker images -a
-   docker tag johann8/${_TAG}:${_VERSION} johann8/${_TAG}:latest
+   docker tag johann8/${CUSTOM_TAG}:${CUSTOM_VERSION} johann8/${CUSTOM_TAG}:latest
 fi
 
-#push image to dockerhub
+# push docker image to dockerhub
 if [ ${_BUILD} = 0 ]; then
    echo "Pushing docker images to dockerhub..."
-   docker push johann8/${_TAG}:latest
-   docker push johann8/${_TAG}:${_VERSION}
+   docker push johann8/${CUSTOM_TAG}:latest
+   docker push johann8/${CUSTOM_TAG}:${CUSTOM_VERSION}
    _PUSH=$?
-   docker images -a |grep ocs
-fi
-
-#
-### build docker contsiner image glpi crond
-#
-if [ ${_PUSH} = 0 ]; then
-   echo "Pushing docker image \"${_TAG}\" was successfull."
-else
-   echo "ERROR: Pushing docker image \"${_TAG}\" was not successfull."
+   docker images -a | grep openldap
 fi
 
 #delete build
 if [ ${_PUSH} = 0 ]; then
    echo "Deleting docker images..."
-   docker rmi johann8/${_TAG}:latest
-   docker rmi johann8/${_TAG}:${_VERSION}
+   docker rmi johann8/${CUSTOM_TAG}:latest
+   docker rmi johann8/${CUSTOM_TAG}:${CUSTOM_VERSION}
    #docker rmi $(docker images -f "dangling=true" -q)
    docker images -a
 fi
-
-# Delete none images
-# docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
