2807f9a0d173920236b1d8d62a7ea945a757cc36
johann8 <jo.hahn@gmx.de>
2026-06-18 10:09:54 +0200

    changed: openldap version to 2.6.13-r0; docker image version to 0.2.8

--- build_image.sh
+++ build_image.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 # set variables
-CUSTOM_VERSION=0.2.7
-CUSTOM_TAG=alpine-openldap
+D_IMAGE_VERSION=0.2.8
+D_IMAGE_TAG=alpine-openldap
+OPENLDAP_VERSION=2.6.13-r0
 
 # build image openldap
 DOCKER_BUILDKIT=0; 
 
 docker build \
-  --build-arg=OPENLDAP_VERSION=2.6.10-r0 \
-  --tag=johann8/${CUSTOM_TAG}:${CUSTOM_VERSION} \
+  --build-arg=OPENLDAP_VERSION=${OPENLDAP_VERSION} \
+  --platform=linux/amd64 \
+  --tag=johann8/${D_IMAGE_TAG}:${D_IMAGE_VERSION} \
   --file=Dockerfile . 2>&1 | tee ./build.log
 
 # Result of build
@@ -22,14 +24,14 @@ if ! [ ${_BUILD} = 0 ]; then
 else
    echo "Docker Image build successful"
    docker images -a
-   docker tag johann8/${CUSTOM_TAG}:${CUSTOM_VERSION} johann8/${CUSTOM_TAG}:latest
+   docker tag johann8/${D_IMAGE_TAG}:${D_IMAGE_VERSION} johann8/${D_IMAGE_TAG}:latest
 fi
 
 # push docker image to dockerhub
 if [ ${_BUILD} = 0 ]; then
    echo "Pushing docker images to dockerhub..."
-   docker push johann8/${CUSTOM_TAG}:latest
-   docker push johann8/${CUSTOM_TAG}:${CUSTOM_VERSION}
+   docker push johann8/${D_IMAGE_TAG}:latest
+   docker push johann8/${D_IMAGE_TAG}:${D_IMAGE_VERSION}
    _PUSH=$?
    docker images -a | grep openldap
 fi
@@ -37,8 +39,8 @@ fi
 #delete build
 if [ ${_PUSH} = 0 ]; then
    echo "Deleting docker images..."
-   docker rmi johann8/${CUSTOM_TAG}:latest
-   docker rmi johann8/${CUSTOM_TAG}:${CUSTOM_VERSION}
+   docker rmi johann8/${D_IMAGE_TAG}:latest
+   docker rmi johann8/${D_IMAGE_TAG}:${D_IMAGE_VERSION}
    #docker rmi $(docker images -f "dangling=true" -q)
    docker images -a
 fi
